9843f000ad7f9acbb612a211fab7236dcd75ad5b
benhorst <benpub@benhorst.com>
2020-11-16 20:45:24 -0800

    add a link from homepage

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,17 @@ const Home = () => {
 
       <Container className="vertical-fill-box">
         <main className="flex-grow">
-          <Segment attached="bottom">Header</Segment>
+          <Segment attached="bottom">Test of some SOLID SDK</Segment>
           <br />
 
-          <Segment>Some placeholder.</Segment>
+          <Segment>
+            Right now /profile is the only page that really works.{" "}
+            <a href="/profile">try it.</a>
+          </Segment>
         </main>
 
         <footer>
-          <Segment attached="top">footer.</Segment>
+          <Segment attached="top">footer. cool.</Segment>
         </footer>
       </Container>
     </div>
